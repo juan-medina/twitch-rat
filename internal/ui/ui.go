4c30fe1fc4dbcf38361d65163c344069a24e818f
Juan Medina <jamedina@gmail.com>
2024-05-30 15:14:22 +0100

    adding button

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -25,11 +25,58 @@ package ui
 import (
 	"bytes"
 	"embed"
+	"fmt"
+	"image/color"
 	"io/fs"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+var (
+	yellow      = color.RGBA64{0xFFFF, 0xFFFF, 0x0000, 0xFFFF}
+	lightPurple = color.RGBA64{0xFFFF, 0x0000, 0xFFFF, 0xFFFF}
+	darkPurple  = color.RGBA64{0x6666, 0x0000, 0x6666, 0xFFFF}
+	purple      = color.RGBA64{0x8888, 0x0000, 0x8888, 0x0000}
+
+	enabledColor = darkPurple
+	hoverColor   = purple
+	pressedColor = lightPurple
+
+	buttonTextColor = yellow
+)
+
+type buttonState int
+
+const (
+	buttonDisabled buttonState = iota
+	buttonEnabled
+	buttonHover
+	buttonPressed
+)
+
+type ButtonId int
+
+const (
+	CONNECT_BUTTON ButtonId = iota
+)
+
+type button struct {
+	id         ButtonId
+	x, y, w, h float64
+	label      string
+	color      color.Color
+	visible    bool
+	do         text.DrawOptions
+	state      buttonState
+}
+
+const (
+	MAX_BUTTONS   = 10
+	BUTTON_WIDTH  = 150
+	BUTTON_HEIGHT = 50
 )
 
 type UI interface {
@@ -47,6 +94,7 @@ type ui struct {
 	normalFace    *text.GoTextFace
 	lastMessage   string
 	lastMessageDO text.DrawOptions
+	buttons       []button
 }
 
 // init implements UI.
@@ -73,25 +121,100 @@ func (u *ui) Init(fileSystem embed.FS, width int, height int) {
 	}
 
 	u.lastMessageDO.GeoM.Reset()
-
 	gapX := u.normalFace.Size
 	gapY := u.normalFace.Size * 1.5
-
 	u.lastMessageDO.GeoM.Translate(gapX, float64(u.screenHeight)-gapY)
 	u.lastMessage = "Loading..."
+
+	u.buttons = make([]button, 0, MAX_BUTTONS)
+
+	bx := float64((u.screenWidth / 2) - (BUTTON_WIDTH / 2))
+	by := float64((u.screenHeight / 2) - (BUTTON_HEIGHT / 2))
+	u.addButton(CONNECT_BUTTON, bx, by, BUTTON_WIDTH, BUTTON_HEIGHT, "CONNECT")
 }
 
 // Draw implements UI.
 func (u *ui) Draw(screen *ebiten.Image) {
 	text.Draw(screen, u.lastMessage, u.normalFace, &u.lastMessageDO)
+	for _, b := range u.buttons {
+		if b.visible {
+			vector.DrawFilledRect(screen, float32(b.x), float32(b.y), float32(b.w), float32(b.h), b.color, false)
+			text.Draw(screen, b.label, u.normalFace, &b.do)
+		}
+	}
 }
 
 // Update implements UI.
 func (u *ui) Update() {
+	x, y := ebiten.CursorPosition()
+	pressed := ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft)
+	for i, b := range u.buttons {
+		if b.visible {
+			if b.state != buttonDisabled {
+				if u.buttonHit(b, float64(x), float64(y)) {
+					if pressed {
+						if b.state != buttonPressed {
+							fmt.Printf("button %d pressed", b.id)
+							u.buttons[i].state = buttonPressed
+						}
+					} else {
+						u.buttons[i].state = buttonHover
+					}
+				} else {
+					u.buttons[i].state = buttonEnabled
+				}
+			}
+			switch b.state {
+			case buttonHover:
+				u.buttons[i].color = hoverColor
+			case buttonEnabled:
+				u.buttons[i].color = enabledColor
+			case buttonPressed:
+				u.buttons[i].color = pressedColor
+			}
+		}
+	}
 }
 
 func (u *ui) SetStatusMessage(message string) {
 	u.lastMessage = message
+}
+
+func (u *ui) addButton(id ButtonId, x, y, w, h float64, label string) {
+	do := text.DrawOptions{}
+	do.ColorScale.Reset()
+	r := float32(buttonTextColor.R / 255.0)
+	g := float32(buttonTextColor.G / 255.0)
+	b := float32(buttonTextColor.B / 255.0)
+	a := float32(buttonTextColor.A / 255.0)
+	do.ColorScale.Scale(r, g, b, a)
+
+	dx, dy := text.Measure(label, u.normalFace, 0)
+	tx := x + (w / 2) - (dx / 2)
+	ty := y + (h / 2) - (dy / 2)
+
+	do.GeoM.Reset()
+	do.GeoM.Translate(tx, ty)
+
+	u.buttons = append(u.buttons, button{
+		id:      id,
+		x:       x,
+		y:       y,
+		w:       w,
+		h:       h,
+		label:   label,
+		color:   enabledColor,
+		visible: true,
+		do:      do,
+		state:   buttonEnabled,
+	})
+}
+
+func (u ui) buttonHit(b button, x, y float64) bool {
+	if x > b.x && x < b.x+BUTTON_WIDTH && y > b.y && y < b.y+BUTTON_HEIGHT {
+		return true
+	}
+	return false
 }
 
 func New() UI {
